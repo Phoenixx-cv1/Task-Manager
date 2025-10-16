ac79f7e71e2c0c65c23432390001ec8d7d145bd8
Phoenixx-cv1 <nishalibss11@gmail.com>
2025-10-16 15:19:16 +0530

    Fix -credentials_exception variable name

--- backend/upauth.py
+++ backend/upauth.py
@@ -18,7 +18,7 @@ def get_db():
     db.close()
 
 def get_current_user(token:str= Depends(oauth2_scheme), db:Session= Depends(get_db)):
-   credentials_expection= HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials",headers={"WWW-Authenticate":"Bearer"},)
+   credentials_exception= HTTPException(status_code= status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials",headers={"WWW-Authenticate":"Bearer"},)
    try:
     payload= jwt.decode(token,SECRET_KEY, algorithms=[ALGORITHM])
     username: str=payload.get("sub")
